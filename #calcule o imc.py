9e22a63f276297c1690a2f962e28342f29e6e1fa
claracosta19 <mariaclaracosta064@gmail.com>
2024-11-19 09:43:34 -0300

    Add files via upload

---
+++ #calcule o imc.py
@@ -0,0 +1,23 @@
+#calcule o imc
+'''peso= int(input('informe seu peso:'))
+altura=float (input('informe sua altura:'))
+def calc_imc (peso, altura):
+    return peso / altura**2 
+
+imc= calc_imc (peso, altura )
+print('seu imc é', imc)'''
+
+#calcule impar/par
+def calc_impar_par (numero):
+    if numero % 2 == 0:
+        print('esse numero é par')
+    else:
+        print('esse numero é impar')
+
+numero=int(input('informe seu numero:'))
+calc_impar_par(numero)
+
+
+
+
+
